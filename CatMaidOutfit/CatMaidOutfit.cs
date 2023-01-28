aaaca7f770cd80bf49c6389346ad244c0ad96827
quackandcheese <8crackedmc8@gmail.com>
2023-01-27 21:18:20 -0600

    v0.1.0

---
+++ CatMaidOutfit/CatMaidOutfit.cs
@@ -0,0 +1,28 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using UnityEngine;
+
+namespace KitchenCatMaidOutfit
+{
+    public class CatMaidOutfit : CustomPlayerCosmetic
+    {
+        public override string UniqueNameID => "catmaidoutfit";
+        public override CosmeticType CosmeticType => CosmeticType.Outfit;
+        public override GameObject Visual => Main.bundle.LoadAsset<GameObject>("CatMaidOutfit");
+
+        public override void OnRegister(GameDataObject gameDataObject)
+        {
+            GameObject Prefab = ((PlayerCosmetic)gameDataObject).Visual;
+
+            PlayerOutfitComponent playerOutfitComponent = Prefab.AddComponent<PlayerOutfitComponent>();
+            playerOutfitComponent.Renderers.Add(GameObjectUtils.GetChildObject(Prefab, "Cat Maid Dress").GetComponent<SkinnedMeshRenderer>());
+
+            MaterialUtils.ApplyMaterial<SkinnedMeshRenderer>(Prefab, "Cat Maid Dress", new Material[] {
+                MaterialUtils.GetExistingMaterial("Clothing Black"),
+                MaterialUtils.GetExistingMaterial("Clothing White")
+            });
+        }
+    }
+}
---
+++ CatMaidOutfit/MoveToModsFolder.bat
@@ -0,0 +1,1 @@
+xcopy /Y "WorkshopFolder" "C:\Program Files (x86)\Steam\steamapps\common\PlateUp\PlateUp\Mods\CatMaidOutfit" /s /e /i
